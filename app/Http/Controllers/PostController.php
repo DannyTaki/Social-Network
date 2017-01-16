269e72699a36a9f5329863d58ef0fc8acf9ae046
Daniel Taki <danieltaki97@gmail.com>
2017-01-16 14:19:19 -0600

    First Commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+use Auth;
+
+class PostController extends Controller
+{
+
+  public function getDashboard() {
+    $posts = Post::orderBy('created_at', 'desc')->get();
+    return view('dashboard', ['posts' => $posts]);
+  }
+
+  public function postCreatePost(Request $request) {
+    $this->validate($request, [
+      'body' => 'required|max:1000'
+    ]);
+    $post = new Post();
+    $post->body = $request['body'];
+    $message = 'There was an error.';
+    if ($request->user()->posts()->save($post)) {
+      $message = 'Post succesfully created!';
+    };
+    return redirect()->route('dashboard')->with(['message' => $message]);
+  }
+
+  public function getDeletePost($post_id) {
+    $post = Post::where('id', $post_id)->first();
+    if (Auth::user() != $post->user) {
+      return redirect()->back();
+    }
+    $post->delete();
+    return redirect()->route('dashboard')->with(['message' => 'Successfully deleted!']);
+  }
+
+  public function postEditPost(Request $request) {
+    $this->validate($request, [
+      'body' => 'required'
+    ]);
+    $post = Post::find($request['postId']);
+    if(Auth::user() != $post->user) {
+      return redirect()->back();
+    }
+    $post->body = $request['body'];
+    $post->update();
+    return response()->json(['message' => 'Post edited!'], 200);
+  }
+}
